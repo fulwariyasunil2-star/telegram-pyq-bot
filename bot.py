689ccd33aedd0f2c1a9bc0ba0e8204574bee05b0
fulwariyasunil2-star <fulwariyasunil2@gmail.com>
2026-07-14 08:36:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import os
-from telegram import Update
+from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🙏 Welcome to Rajasthan Exam PYQ Bot!\n\n"
-        "जल्द ही यहाँ राजस्थान की सभी परीक्षाओं के PYQ उपलब्ध होंगे।"
+        "जल्द ही यहाँ राजस्थान की सभी परीक्षाओं के PYQ उपलब्ध होंगे।",
+        reply_markup=ReplyKeyboardRemove()
     )
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
